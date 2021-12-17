5312f103133a5f76a7102aacd1f6cabdb8cc323f
09091998 <96254081+09091998@users.noreply.github.com>
2021-12-17 18:13:14 +0530

    Add files via upload

---
+++ maxbt2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+     int n = 15,i = 16;
+if (n >= i)
+cout << "n is maximum number";
+else
+cout << "i is maximum number";
+
+    return 0;
+}
